09514066060a5b76482540ab951320c4ed45bdd1
Adella <adellakenn@gmail.com>
2019-05-22 21:47:36 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,13 @@
-classsssssssssssssssss erso
+class Person
+  def initialize(name)
+    @name = name
+  end
+  
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name
+  end
+end
